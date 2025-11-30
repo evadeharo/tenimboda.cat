354da13b1c59c358941a7fb95bcd1dc431729121
Eva De Haro <evadeharomunoz@gmail.com>
2025-11-30 18:42:32 +0100

    fix: compress images

--- src/components/Present.tsx
+++ src/components/Present.tsx
@@ -86,6 +86,7 @@ export default function Present() {
               animate={{ opacity: 1 }}
               exit={{ opacity: 0 }}
               transition={{ duration: 0.3 }}
+              loading="eager"
             />
           </div>
         )}
